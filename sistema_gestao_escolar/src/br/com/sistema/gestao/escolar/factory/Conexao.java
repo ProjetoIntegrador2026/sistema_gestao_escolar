1d135ff1bff07364eaeff19a9b90fbde6c0dc82f
Rubens Bitencourt <contato@rubensconsult.com.br>
2026-06-30 14:55:08 -0300

    atualizacao da conexao junto a nuvem ultima versao

--- sistema_gestao_escolar/src/br/com/sistema/gestao/escolar/factory/Conexao.java
+++ sistema_gestao_escolar/src/br/com/sistema/gestao/escolar/factory/Conexao.java
@@ -10,18 +10,17 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    // URL ajustada com o formato JDBC que o Java exige + parâmetro de segurança SSL
-    private static final String URL = "jdbc:postgresql://dpg-d91g8q28qa3s73aiu700-a.oregon-postgres.render.com/meubanco_java?sslmode=require";
+    // FIX: URL Externa exata da Render no formato JDBC com a porta :5432 e SSL ativo
+    private static final String URL = "jdbc:postgresql://dpg-d91g8q28qa3s73aiu700-a.oregon-postgres.render.com:5432/meubanco_java?sslmode=require";
     private static final String USUARIO = "meubanco_java_user";
     private static final String SENHA = "2dPetn34fYKTmIWD8HCBCfXkHgCi78t5";
 
     public static Connection getConexao() throws SQLException {
         try {
-            // Carrega o driver físico postgresql-42.7.11.jar que está na sua pasta lib
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(URL, USUARIO, SENHA);
         } catch (ClassNotFoundException e) {
-            throw new SQLException("Erro: O driver postgresql-42.7.11.jar não foi encontrado na pasta lib!", e);
+            throw new SQLException("Erro: O driver postgresql não foi encontrado na biblioteca!", e);
         }
     }
 }
